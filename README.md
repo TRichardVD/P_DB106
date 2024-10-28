4d2c24085c609a274b8466bf6417033fb11a3143
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2024-10-28 14:48:43 +0100

    doc(Noms fichiers): Modification des noms de fichiers pour respecter les conventions de nommages

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Ce projet est réalisé durant un projet se nommant P_DB 106 à l'[ETML](https:/
 *Pour pouvoir rédiger mes réponses j'ai utilisé le format markdown et pour me faciliter l'édition et la rédaction j'ai utilisé l'éditeur markdown ["Obsidian"](https://www.obsidian.md) qui est gratuit.* 
 
 ## Fichiers du repository
-- [Document final répondant à toutes les exigences du cahier des charges](Travail%20effectué.md) : Explications de toutes les commandes effectuées durant ce projet
-- [Fichier bat permettant d'exécuter tous les scripts SQL demandés](./script/ExecutionScripts.bat)
-- [Toutes les consignes du projet](./consignes) (Cahier des charges, etc.)
+- [Document final répondant à toutes les exigences du cahier des charges](./Doc/D-P_DB106-TRD-TravailEffectue.md) : Explications de toutes les commandes effectuées durant ce projet
+- [Fichier bat permettant d'exécuter tous les scripts SQL demandés](./script/_Y-P_DB106-TRD-ExecutionScripts.bat)
+- [Toutes les consignes du projet](./consignes/) (Cahier des charges, etc.)
 
 ## Annexes
 https://etml.icescrum.com/p/PDB106/
--- scripts/_Y-P_DB106-TRD-ExecutionScripts.bat
+++ scripts/_Y-P_DB106-TRD-ExecutionScripts.bat
@@ -1,20 +1,20 @@
 :: Effacer les rôles etc. si déjà créés
-docker exec -i db mysql -uroot -proot < prerequis.sql
+docker exec -i db mysql -uroot -proot < Y-P_DB106-TRD-prerequis.sql
 
 :: Importation du dump de base de la base de donneées db_space_invaders.
 docker exec -i db mysql -uroot -proot < db_space_invaders_Base.sql
 
 
 :: Excecution du script créés pour répondre au éxigence du cahier des charges liés à la gestion des rôles et des utilisateurs.
-docker exec -i db mysql -uroot -proot db_space_invaders < users.sql
+docker exec -i db mysql -uroot -proot db_space_invaders < Y-P_DB106-TRD-users.sql
 
 
 :: Execution des requêtes de sélections
-docker exec -i db mysql -uroot -proot db_space_invaders < requetes.sql
+docker exec -i db mysql -uroot -proot db_space_invaders < Y-P_DB106-TRD-requetes.sql
 
 
 :: Création des indexes
-docker exec -i db mysql -uroot -proot db_space_invaders < index.sql
+docker exec -i db mysql -uroot -proot db_space_invaders < Y-P_DB106-TRD-index.sql
 
 
 PAUSE
@@ -25,7 +25,7 @@ docker exec -i db mysqldump -uroot -proot db_space_invaders > ./Backup/db_space_
 PAUSE
 
 :: Effacer la base de données, les utilisateurs et les rôles
-docker exec -i db mysql -uroot -proot < prerequis.sql
+docker exec -i db mysql -uroot -proot < Y-P_DB106-TRD-prerequis.sql
 
 :: Restaurer le dump de la base de données
 docker exec -i db mysql -uroot -proot < ./Backup/db_space_invaders.sql
